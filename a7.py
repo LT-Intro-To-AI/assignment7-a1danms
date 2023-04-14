397f129bc7f29d84fae2d2286a6a51e878e22453
Aidan Sheridan <aidansheridan100@gmail.com>
2023-04-14 09:33:56 -0500

    voter opinion

--- a7.py
+++ a7.py
@@ -10,4 +10,18 @@ orn = NeuralNet (2, 3 1)
 
 
 
+print("\n\nTraining voter opinion")
+print()
+voter_opinion = [
+    ([.9, .6, .8, .3, .1], [1])
+    ([.8, .8, .4, .6, .4], [1])
+    ([.7, .2, .4, .6, .3], [1])
+    ([.5, .5, .8, .4, .8], [0])
+    ([.3, .1, .6, .8, .8], [0])
+    ([.6, .3, .4, .3, .6], [0])]
+
+von = NeuralNet(5, 6, 1)
+
+von.train(voter_opinion) 
+print(von.test_with_expected(voter_opinion))
 
